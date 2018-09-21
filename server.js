af56524b47d44967c7da8facd3abc538ad540428
steveguo <380188469@qq.com>
2018-09-22 00:13:40 +0200

    fix homepage

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 app.set('port', (5000))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/build/index.html')
+    res.sendFile(process.cwd() +'/build/index.html')
 })
 
 app.post('/prices/new', (req, res) => {
